5349478203c6778cb1be4c51af23a28ada6b1952
Tim <timour.almazov@polyconseil.fr>
2022-06-27 14:06:20 +0200

    new contract addr

--- src/hooks/game.ts
+++ src/hooks/game.ts
@@ -5,6 +5,6 @@ import abi from './abi/Ogame.json'
 export function useGameContract() {
   return useContract({
     abi: abi as Abi,
-    address: '0x0193b22090a2e09676719b0708101e4f6890f17c430303495f58c83cc5b6b0b4',
+    address: '0x05899384efed742ca37a8d17727f1501bcbe99f185001911351e5f86798bdc28',
   })
 }
